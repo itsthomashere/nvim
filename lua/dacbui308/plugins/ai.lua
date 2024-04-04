74c7d82c344875d81cc45e1436920242da87e457
BMHDAC <dac.bui308@gmail.com>
2024-04-04 23:21:07 +0700

    AI

--- lua/dacbui308/plugins/ai.lua
+++ lua/dacbui308/plugins/ai.lua
@@ -4,14 +4,8 @@ return {
 	config = function()
 		-- Change '<C-g>' here to any keycode you like.
 		vim.g.codeium_disable_bindings = 1
-		vim.keymap.set("i", "<C-m>", function()
+		vim.keymap.set("i", "<C-g>", function()
 			return vim.fn["codeium#Accept"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-;>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-'>", function()
-			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true, silent = true })
 		vim.keymap.set("i", "<c-x>", function()
 			return vim.fn["codeium#Clear"]()
