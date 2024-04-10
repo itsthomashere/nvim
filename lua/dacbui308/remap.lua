218bcc5a5da0cb822cfa1a0593b9ae00a8bd23b3
BMHDAC <dac.bui308@gmail.com>
2024-04-11 06:01:48 +0700

    Error prompt for easy usecases

--- lua/dacbui308/remap.lua
+++ lua/dacbui308/remap.lua
@@ -23,6 +23,8 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 vim.keymap.set("n", "<leader>e", vim.cmd.bnext)
 vim.keymap.set("n", "<leader>q", vim.cmd.bprevious)
 vim.keymap.set("n", "<leader>x", vim.cmd.bd)
+vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>lf", vim.diagnostic.setloclist)
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
