ebc8aa2168be87e2111cf13ad0856736a4187a38
Thomas <dac.bui308@gmail.com>
2024-05-05 19:05:37 +0700

    telescope

--- lua/dacbui308/plugins/telescope.lua
+++ lua/dacbui308/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
 	end,
