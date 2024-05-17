877831d1db6257c32b746a8c7654b72f4f11f701
Thomas <dac.bui308@gmail.com>
2024-05-17 21:27:45 +0700

    Debugger for c++ and rust

--- lua/dacbui308/plugins/debugtools.lua
+++ lua/dacbui308/plugins/debugtools.lua
@@ -5,7 +5,7 @@ return {
 		require("neodev").setup({
 			library = { plugins = { "nvim-dap-ui" }, types = true },
 		})
-
+		require("dapui").setup()
 		local dap, dapui = require("dap"), require("dapui")
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
@@ -20,20 +20,39 @@ return {
 			dapui.close()
 		end
 
-		vim.keymap.set("n", "<leader>dbp", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<leader>dbc", dap.continue, {})
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<leader>dx", dap.disconnect, {})
+		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<leader>do", dap.step_over, {})
+		vim.keymap.set("n", "<leader>di", dap.step_into, {})
+		vim.keymap.set("n", "<leader>dO", dap.step_out, {})
+		vim.keymap.set("n", "<leader>dr", dap.repl.open, {})
+		vim.keymap.set("n", "<leader>dl", dap.run_last, {})
 
 		dap.adapters.codelldb = {
 			type = "server",
 			port = "${port}",
 			executable = {
 				-- CHANGE THIS to your path!
-				command = "~/.local/share/nvim/mason/packages/codelldb/codelldb",
+				command = "/home/dacbui308/.local/share/nvim/mason/packages/codelldb/extension/adapter/codelldb",
 				args = { "--port", "${port}" },
 
 				-- On windows you may have to uncomment this:
 				-- detached = false,
 			},
 		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+			},
+		}
+		dap.configurations.rust = dap.configurations.cpp
 	end,
 }
