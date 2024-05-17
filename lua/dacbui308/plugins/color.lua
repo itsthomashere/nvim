c53bcde155e0ba392b168a5d16b6d87214a87b24
Thomas <dac.bui308@gmail.com>
2024-05-17 18:43:19 +0700

    update

--- lua/dacbui308/plugins/color.lua
+++ lua/dacbui308/plugins/color.lua
@@ -1,19 +1,15 @@
 return {
-	"folke/tokyonight.nvim",
+	"rose-pine/neovim",
+	name = "rose-pine",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			transparent = true,
-		})
-		vim.cmd.colorscheme("tokyonight")
+		require("rose-pine").setup()
 		function Transparent(color)
-			color = color or "tokyonight"
+			color = color or "rose-pine-main"
 			vim.cmd.colorscheme(color)
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none", ctermfg = "none" })
 			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", ctermfg = "none" })
-
 			vim.api.nvim_set_hl(0, "NormalSB", { bg = "none", ctermfg = "none" })
 		end
 		Transparent()
