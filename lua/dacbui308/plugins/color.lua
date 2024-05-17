17948f99885ae17245d96e4a3bbafcdfe0728ed9
Thomas <dac.bui308@gmail.com>
2024-05-17 18:58:35 +0700

    update

--- lua/dacbui308/plugins/color.lua
+++ lua/dacbui308/plugins/color.lua
@@ -1,16 +1,43 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	lazy = false,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		require("rose-pine").setup()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = false, -- disables setting the background color. show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+			term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = false, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+			},
+			no_underline = false, -- Force no underline
+			default_integrations = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+			},
+		})
 		function Transparent(color)
-			color = color or "rose-pine-main"
+			color = color or "catppuccin"
 			vim.cmd.colorscheme(color)
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none", ctermfg = "none" })
 			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", ctermfg = "none" })
 			vim.api.nvim_set_hl(0, "NormalSB", { bg = "none", ctermfg = "none" })
+			vim.api.nvim_set_hl(0, "CursorLine", { undercurl = true })
 		end
 		Transparent()
 	end,
