f255d8fe1c67239d4c7b651befd8b0167ff13806
Thomas <dac.bui308@gmail.com>
2024-05-18 13:22:06 +0700

    Update gitsigns intergration

--- lua/dacbui308/plugins/gitsigns.lua
+++ lua/dacbui308/plugins/gitsigns.lua
@@ -19,7 +19,7 @@ return {
 			},
 			auto_attach = true,
 			attach_to_untracked = false,
-			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 			current_line_blame_opts = {
 				virt_text = true,
 				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -43,6 +43,34 @@ return {
 			yadm = {
 				enable = false,
 			},
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end)
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end)
+				map("n", "<leader>gp", gitsigns.preview_hunk)
+				map("n", "<leader>gl", gitsigns.toggle_current_line_blame)
+			end,
 		})
 	end,
 }
