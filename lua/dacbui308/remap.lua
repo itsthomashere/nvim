5a0e687891baf63df8d54ecc450f1df76d04f9cc
Thomas <bdachai308@gmail.com>
2024-07-03 20:06:30 +0700

    Update Some config

--- lua/dacbui308/remap.lua
+++ lua/dacbui308/remap.lua
@@ -45,14 +45,7 @@ function Transarent(color)
 	end
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-	vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-end
-function TurnOffHighlight()
-	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", ctermfg = "none" })
-	vim.api.nvim_set_hl(0, "NormalSB", { bg = "none", ctermfg = "none" })
 	vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
 	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
