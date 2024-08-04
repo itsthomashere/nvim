1fdab19755816ced8ab1610a87fdf7591e4eeb49
Thomas <bdachai308@gmail.com>
2024-08-04 22:48:50 +0700

    Fix netrw bugs

--- lua/dacbui308/plugins/mini.lua
+++ lua/dacbui308/plugins/mini.lua
@@ -23,7 +23,6 @@ return {
 		require("mini.indentscope").setup()
 		require("mini.icons").setup()
 		require("mini.statusline").setup()
-		require("mini.tabline").setup()
 		require("mini.icons").mock_nvim_web_devicons()
 		require("mini.bufremove").setup()
 		require("mini.notify").setup({
