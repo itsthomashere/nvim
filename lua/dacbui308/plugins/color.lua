053eb52bec6583b85002cbcd85b78614cc4629d9
BMHDAC <bdachai308@gmail.com>
2024-08-25 21:43:04 +0700

    mend

--- lua/dacbui308/plugins/color.lua
+++ lua/dacbui308/plugins/color.lua
@@ -19,41 +19,36 @@ return {
 	-- 		Transparent("eldritch")
 	-- 	end,
 	-- },
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	priority = 1000,
-	-- 	name = "rose-pine",
-	-- 	config = function()
-	-- 		require("rose-pine").setup({
-	-- 			styles = {
-	-- 				italic = true,
-	-- 				transparency = false,
-	-- 			},
-	-- 			highlight_groups = {
-	-- 				Keyword = { italic = true },
-	-- 				Function = { italic = true },
-	-- 				-- Conditional = { italic = true },
-	-- 				StatusLine = { fg = "iris", bg = "iris", blend = 10 },
-	-- 				StatusLineNC = { fg = "subtle", bg = "surface" },
-	-- 				TelescopeBorder = { fg = "overlay", bg = "overlay" },
-	-- 				TelescopeNormal = { fg = "subtle", bg = "overlay" },
-	-- 				TelescopeSelection = { fg = "text", bg = "highlight_med" },
-	-- 				TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
-	-- 				TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
-	--
-	-- 				TelescopeTitle = { fg = "base", bg = "love" },
-	-- 				TelescopePromptTitle = { fg = "base", bg = "pine" },
-	-- 				TelescopePreviewTitle = { fg = "base", bg = "iris" },
-	--
-	-- 				TelescopePromptNormal = { fg = "text", bg = "surface" },
-	-- 				TelescopePromptBorder = { fg = "surface", bg = "surface" },
-	-- 			},
-	-- 		})
-	-- 		vim.opt.laststatus = 2 -- Or 3 for global statusline
-	-- 		vim.opt.statusline = " %f %m %= %l:%c ♥ "
-	-- 		Transparent("rose-pine")
-	-- 	end,
-	-- },
+	{
+		"rose-pine/neovim",
+		priority = 1000,
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				styles = {
+					italic = false,
+					transparency = false,
+				},
+				highlight_groups = {
+					StatusLine = { fg = "iris", bg = "iris", blend = 10 },
+					StatusLineNC = { fg = "subtle", bg = "surface" },
+					TelescopeBorder = { fg = "overlay", bg = "overlay" },
+					TelescopeNormal = { fg = "subtle", bg = "overlay" },
+					TelescopeSelection = { fg = "text", bg = "highlight_med" },
+					TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+					TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+
+					TelescopeTitle = { fg = "base", bg = "love" },
+					TelescopePromptTitle = { fg = "base", bg = "pine" },
+					TelescopePreviewTitle = { fg = "base", bg = "iris" },
+
+					TelescopePromptNormal = { fg = "text", bg = "surface" },
+					TelescopePromptBorder = { fg = "surface", bg = "surface" },
+				},
+			})
+			Transparent("rose-pine")
+		end,
+	},
 	-- {
 	-- 	"rebelot/kanagawa.nvim",
 	-- 	config = function()
@@ -132,6 +127,13 @@ return {
 	-- 	end,
 	-- },
 	-- {
+	-- 	"Mofiqul/vscode.nvim",
+	-- 	config = function()
+	-- 		require("vscode").setup({})
+	-- 		vim.cmd.colorscheme("vscode")
+	-- 	end,
+	-- },
+	-- {
 	-- 	"olimorris/onedarkpro.nvim",
 	-- 	priority = 1000, -- Ensure it loads first
 	-- 	config = function()
@@ -156,10 +158,10 @@ return {
 	-- 		Transparent("tokyonight")
 	-- 	end,
 	-- },
-	{
-		"RRethy/base16-nvim",
-		config = function()
-			Transparent("base16-humanoid-dark")
-		end,
-	},
+	-- {
+	-- 	"RRethy/base16-nvim",
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("base16-equilibrium-dark")
+	-- 	end,
+	-- },
 }
