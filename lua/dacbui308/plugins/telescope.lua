a48f4213cb27e2b265c6e8ac86b8ea97fb5bc785
BMHDAC <bdachai308@gmail.com>
2024-08-28 23:19:14 +0700

    Remove useless plugins

--- lua/dacbui308/plugins/telescope.lua
+++ lua/dacbui308/plugins/telescope.lua
@@ -4,7 +4,6 @@ return {
 	event = "VimEnter",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope-ui-select.nvim" },
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
