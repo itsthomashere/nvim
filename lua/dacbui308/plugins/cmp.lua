ad2a3c09642c026bdb8ccadcfa5964c074f88bd9
BMHDAC <bdachai308@gmail.com>
2024-08-30 00:35:27 +0700

    Add lua snip

--- lua/dacbui308/plugins/cmp.lua
+++ lua/dacbui308/plugins/cmp.lua
@@ -4,11 +4,38 @@ return { -- Autocompletion
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
+		{
+			"L3MON4D3/LuaSnip",
+			build = (function()
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- `friendly-snippets` contains a variety of premade snippets.
+				--    See the README about individual language/framework/plugin snippets:
+				--    https://github.com/rafamadriz/friendly-snippets
+				-- {
+				--   'rafamadriz/friendly-snippets',
+				--   config = function()
+				--     require('luasnip.loaders.from_vscode').lazy_load()
+				--   end,
+				-- },
+			},
+		},
+		"saadparwaiz1/cmp_luasnip",
 	},
 	config = function()
 		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		luasnip.setup({})
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 		cmp.setup({
+			---@diagnostic disable-next-line: missing-fields
 			performance = {
 				max_view_entries = 10,
 			},
@@ -64,7 +91,7 @@ return { -- Autocompletion
 			},
 			snippet = {
 				expand = function(args)
-					vim.snippet.expand(args.body)
+					luasnip.lsp_expand(args.body)
 				end,
 			},
 			completion = { completeopt = "menu,menuone,noinsert, select" },
@@ -83,16 +110,17 @@ return { -- Autocompletion
 				end, { "i", "s" }),
 
 				["<C-l>"] = cmp.mapping(function()
-					if vim.snippet.active({ direction = 1 }) then
-						vim.snippet.jump(1)
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
 					end
 				end, { "i", "s" }),
 				["<C-h>"] = cmp.mapping(function()
-					if vim.snippet.active({ direction = -1 }) then
-						vim.snippet.jump(-1)
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
 					end
 				end, { "i", "s" }),
 			}),
+
 			sources = {
 				{ name = "nvim_lsp", priority = 1000 },
 				{ name = "path", priority = 250 },
