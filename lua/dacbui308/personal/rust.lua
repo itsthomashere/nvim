e87ff2acacf0b72224387c44332f2772c6307eca
BMHDAC <bdachai308@gmail.com>
2024-12-11 18:27:24 +0700

    Add syntax tree rust-analyzer

--- lua/dacbui308/personal/rust.lua
+++ lua/dacbui308/personal/rust.lua
@@ -4,7 +4,7 @@ local utils = require("dacbui308.personal.utils")
 local rust = {}
 local latest_buf_id = nil
 
-local function parse_lines(result)
+local function parse_macros(result)
 	local ret = {}
 
 	local name = result.name
@@ -17,6 +17,16 @@ local function parse_lines(result)
 	local expansion = result.expansion
 	for string in string.gmatch(expansion, "([^\n]+)") do
 		table.insert(ret, string)
+	end
+
+	return ret
+end
+
+local function parse_syntax_tree(result)
+	local ret = {}
+
+	for line in string.gmatch(result, "([^\n]+)") do
+		table.insert(ret, line)
 	end
 
 	return ret
@@ -46,9 +56,27 @@ function rust.expand_macro()
 		utils.open_split(true, latest_buf_id)
 
 		vim.bo[latest_buf_id].filetype = "rust"
-		vim.api.nvim_buf_set_lines(latest_buf_id, 0, 0, false, parse_lines(result))
+		vim.api.nvim_buf_set_lines(latest_buf_id, 0, 0, false, parse_macros(result))
 		vim.bo[latest_buf_id].readonly = true
 
+		utils.resize(true, "-25")
+	end)
+end
+
+function rust.syntax_tree()
+	local clients = lsp.get_client(0, {
+		name = "rust_analyzer",
+	})
+	if #clients == 0 then
+		return
+	end
+	local params = vim.lsp.util.make_position_params(0, clients[1].offset_encoding or "utf-8")
+	lsp.buf_request_lsp("rust_analyzer", "rust-analyzer/syntaxTree", params, 0, function(_, result)
+		utils.delete_buf(latest_buf_id)
+		latest_buf_id = vim.api.nvim_create_buf(false, true)
+		utils.open_split(true, latest_buf_id)
+		vim.api.nvim_buf_set_name(latest_buf_id, "syntax.rust")
+		vim.api.nvim_buf_set_text(latest_buf_id, 0, 0, 0, 0, parse_syntax_tree(result))
 		utils.resize(true, "-25")
 	end)
 end
@@ -56,6 +84,9 @@ end
 function rust.setup(config)
 	vim.api.nvim_create_user_command("ExpandMacro", function(args)
 		rust.expand_macro()
+	end, { desc = "Expand macro under the cursor" })
+	vim.api.nvim_create_user_command("SyntaxTree", function(args)
+		rust.syntax_tree()
 	end, { desc = "Expand macro under the cursor" })
 end
 
