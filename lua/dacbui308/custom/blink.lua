2da8e9a8b13b6bc518ac30477d0c0e77a08b3895
BMHDAC <bdachai308@gmail.com>
2024-12-21 03:55:30 +0700

    Update cmp

--- lua/dacbui308/custom/blink.lua
+++ lua/dacbui308/custom/blink.lua
@@ -6,14 +6,49 @@ require("blink.cmp").setup({
 	sources = {
 		default = { "lsp", "path" },
 		providers = {
-			buffer = {},
+			buffer = {
+				enabled = false,
+			},
 		},
+		cmdline = {},
 	},
 	completion = {
 		menu = {
 			draw = {
-				columns = { { "label", "label_description", gap = 1 }, { "kind" } },
+				treesitter = { "lsp" },
+				columns = { { "kind_icon" }, { "label", gap = 1 }, { "kind" } },
+				components = {
+					label = {
+						width = { fill = true, max = 60 },
+						text = function(ctx)
+							return ctx.label
+						end,
+						highlight = function(ctx)
+							local highlights = {
+								{
+									0,
+									#ctx.label,
+									group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+								},
+							}
+							for _, idx in ipairs(ctx.label_matched_indices) do
+								table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+							end
+
+							return highlights
+						end,
+					},
+					source_name = {},
+				},
 			},
 		},
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 0,
+			treesitter_highlighting = true,
+		},
+	},
+	fuzzy = {
+		use_typo_resistance = false,
 	},
 })
